8e458313b84c73fd71b4bd6c45de9fd9d06c9c02
Nick Friday <nfriday@yandex.ru>
2022-06-29 22:14:38 +0300

    fmt & caps

--- keymap.c
+++ keymap.c
@@ -4,12 +4,11 @@
 enum { _DEF = 0, _CLR, _GM0, _GM1, _MOV, _SYM, _WTF };
 
 enum my_keycodes {
-    KC_TGSF = SAFE_RANGE,
-    KC_TGSP,
-    KC_TGAL,
-    KC_TGCT,
-    KC_CLRM,
-    KC_TGMM,
+  KC_TGSF = SAFE_RANGE,
+  KC_TGSP,
+  KC_TGAL,
+  KC_TGCT,
+  KC_TGMM,
 };
 
 #define M_LS(kc) MT(MOD_LSFT, kc)
@@ -60,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_WTF] = LAYOUT_split_3x6_3(
         RESET,  KC_F10,   KC_F7,   KC_F8,   KC_F9,  KC_F20,             KC_TGMM, KC_TGCT, KC_TGAL, KC_TGSP, RGB_HUI, RGB_TOG,
      TG(_WTF),  KC_F11,   KC_F4,   KC_F5,   KC_F6,  KC_F21,             KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR, KC_TGSF, RGB_MOD,
-      KC_CLRM,  KC_F12,   KC_F1,   KC_F2,   KC_F3,  KC_F22,             KC_MSTP, KC_MPRV, KC_MNXT, KC_MPLY, RGB_VAI, RGB_SPI,
+      KC_CAPS,  KC_F12,   KC_F1,   KC_F2,   KC_F3,  KC_F22,             KC_MSTP, KC_MPRV, KC_MNXT, KC_MPLY, RGB_VAI, RGB_SPI,
                                      TT(_CLR), _______, KC_SPC,     KC_ENT, _______, KC_BSPC
   ),
 
@@ -87,20 +86,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 /* clang-format on */
 
-bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case TT(_CLR):
-        case M_LC(KC_J):
-        case M_LA(KC_K):
-        case M_LG(KC_L):
-        case M_LC(KC_DOWN):
-        case M_LA(KC_UP):
-        case M_LG(KC_RGHT):
-        case M_LS(KC_TAB):
-            return false;
-        default:
-            return true;
-    }
+bool get_tapping_force_hold(uint16_t keycode, keyrecord_t* record) {
+  switch (keycode) {
+  case TT(_CLR):
+  case M_LC(KC_J):
+  case M_LA(KC_K):
+  case M_LG(KC_L):
+  case M_LC(KC_DOWN):
+  case M_LA(KC_UP):
+  case M_LG(KC_RGHT):
+  case M_LS(KC_TAB):
+    return false;
+  default:
+    return true;
+  }
 }
 
 bool shift_locked = false;
@@ -108,201 +107,205 @@ bool super_locked = false;
 bool alt_locked   = false;
 bool ctrl_locked  = false;
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case KC_TGSF:
-            if (record->event.pressed) {
-                shift_locked = !shift_locked;
-                if (shift_locked) {
-                    add_mods(MOD_MASK_SHIFT);
-                } else {
-                    del_mods(MOD_MASK_SHIFT);
-                }
-            }
-            return false;
-        case KC_TGSP:
-            if (record->event.pressed) {
-                super_locked = !super_locked;
-                if (super_locked) {
-                    add_mods(MOD_MASK_GUI);
-                } else {
-                    del_mods(MOD_MASK_GUI);
-                }
-            }
-            return false;
-        case KC_TGAL:
-            if (record->event.pressed) {
-                alt_locked = !alt_locked;
-                if (alt_locked) {
-                    add_mods(MOD_MASK_ALT);
-                } else {
-                    del_mods(MOD_MASK_ALT);
-                }
-            }
-            return false;
-        case KC_TGCT:
-            if (record->event.pressed) {
-                ctrl_locked = !ctrl_locked;
-                if (ctrl_locked) {
-                    add_mods(MOD_MASK_CTRL);
-                } else {
-                    del_mods(MOD_MASK_CTRL);
-                }
-            }
-            return false;
-        case KC_CLRM:
-            if (record->event.pressed) {
-                shift_locked = false;
-                super_locked = false;
-                alt_locked   = false;
-                ctrl_locked  = false;
-                clear_mods();
-            }
-            return false;
-        case KC_TGMM:
-            if (record->event.pressed) {
-                shift_locked = true;
-                super_locked = true;
-                alt_locked   = true;
-                ctrl_locked  = true;
-                set_mods(MOD_MASK_CSAG);
-            }
-            return false;
-        default:
-            return true;
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+  switch (keycode) {
+  case KC_TGSF:
+    if (record->event.pressed) {
+      shift_locked = !shift_locked;
+      if (shift_locked) {
+        add_mods(MOD_MASK_SHIFT);
+      } else {
+        del_mods(MOD_MASK_SHIFT);
+      }
     }
+    return false;
+  case KC_TGSP:
+    if (record->event.pressed) {
+      super_locked = !super_locked;
+      if (super_locked) {
+        add_mods(MOD_MASK_GUI);
+      } else {
+        del_mods(MOD_MASK_GUI);
+      }
+    }
+    return false;
+  case KC_TGAL:
+    if (record->event.pressed) {
+      alt_locked = !alt_locked;
+      if (alt_locked) {
+        add_mods(MOD_MASK_ALT);
+      } else {
+        del_mods(MOD_MASK_ALT);
+      }
+    }
+    return false;
+  case KC_TGCT:
+    if (record->event.pressed) {
+      ctrl_locked = !ctrl_locked;
+      if (ctrl_locked) {
+        add_mods(MOD_MASK_CTRL);
+      } else {
+        del_mods(MOD_MASK_CTRL);
+      }
+    }
+    return false;
+  case KC_TGMM:
+    if (record->event.pressed) {
+      if (!get_mods()) {
+        shift_locked = true;
+        super_locked = true;
+        alt_locked   = true;
+        ctrl_locked  = true;
+        set_mods(MOD_MASK_CSAG);
+      } else {
+
+        shift_locked = false;
+        super_locked = false;
+        alt_locked   = false;
+        ctrl_locked  = false;
+        clear_mods();
+      }
+    }
+    return false;
+  default:
+    return true;
+  }
 }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (!is_keyboard_master()) {
-        return OLED_ROTATION_180;
-    }
-    return OLED_ROTATION_270;
+  if (!is_keyboard_master()) {
+    return OLED_ROTATION_180;
+  }
+  return OLED_ROTATION_270;
 }
 
 void oled_render_layer_state(void) {
-    oled_write_P(PSTR("@:"), false);
-    switch (biton32(layer_state)) {
-        case _MOV:
-            oled_write_P(PSTR("mov"), false);
-            break;
-        case _SYM:
-            oled_write_P(PSTR("sym"), false);
-            break;
-        case _WTF:
-            oled_write_P(PSTR("wtf"), false);
-            break;
-        case _CLR:
-            oled_write_P(PSTR("clr"), true);
-            break;
-        case _GM0:
-            oled_write_P(PSTR("gm0"), false);
-            break;
-        case _GM1:
-            oled_write_P(PSTR("gm1"), false);
-            break;
-        default:
-            oled_write_P(PSTR("def"), false);
-            break;
-    }
+  oled_write_P(PSTR("@:"), false);
+  switch (biton32(layer_state)) {
+  case _MOV:
+    oled_write_P(PSTR("mov"), false);
+    break;
+  case _SYM:
+    oled_write_P(PSTR("sym"), false);
+    break;
+  case _WTF:
+    oled_write_P(PSTR("wtf"), false);
+    break;
+  case _CLR:
+    oled_write_P(PSTR("clr"), true);
+    break;
+  case _GM0:
+    oled_write_P(PSTR("gm0"), false);
+    break;
+  case _GM1:
+    oled_write_P(PSTR("gm1"), false);
+    break;
+  default:
+    oled_write_P(PSTR("def"), false);
+    break;
+  }
 }
 
 // through caps lock indicator
 void oled_render_layout(void) {
-    oled_write_P(PSTR("lt:"), false);
-    if (IS_HOST_LED_ON(USB_LED_CAPS_LOCK)) {
-        oled_write_P(PSTR("ru"), true);
-    } else {
-        oled_write_P(PSTR("us"), false);
-    }
+  oled_write_P(PSTR("lt:"), false);
+  if (IS_HOST_LED_ON(USB_LED_NUM_LOCK)) {
+    oled_write_P(PSTR("ru"), true);
+  } else {
+    oled_write_P(PSTR("us"), false);
+  }
 }
 
 void oled_render_rgbinfo(void) {
-    oled_write_P(PSTR("m:"), false);
-    if (rgb_matrix_is_enabled()) {
-        switch (rgb_matrix_get_mode()) {
-            case 2:
-                oled_write_P(PSTR("rsm"), false);
-                break;
-            case 3:
-                oled_write_P(PSTR("rsl"), false);
-                break;
-            case 4:
-                oled_write_P(PSTR("rwd"), false);
-                break;
-            case 5:
-                oled_write_P(PSTR("rnx"), false);
-                break;
-            case 6:
-                oled_write_P(PSTR("spl"), false);
-                break;
-            case 7:
-                oled_write_P(PSTR("ssp"), false);
-                break;
-            default:
-                oled_write_P(PSTR("sld"), false);
-                break;
-        }
-    } else {
-        oled_write_P(PSTR("off"), true);
+  oled_write_P(PSTR("m:"), false);
+  if (rgb_matrix_is_enabled()) {
+    switch (rgb_matrix_get_mode()) {
+    case 2:
+      oled_write_P(PSTR("rsm"), false);
+      break;
+    case 3:
+      oled_write_P(PSTR("rsl"), false);
+      break;
+    case 4:
+      oled_write_P(PSTR("rwd"), false);
+      break;
+    case 5:
+      oled_write_P(PSTR("rnx"), false);
+      break;
+    case 6:
+      oled_write_P(PSTR("spl"), false);
+      break;
+    case 7:
+      oled_write_P(PSTR("ssp"), false);
+      break;
+    default:
+      oled_write_P(PSTR("sld"), false);
+      break;
     }
+  } else {
+    oled_write_P(PSTR("off"), true);
+  }
 
-    char hsv[16];
-    /* clang-format off */
+  char hsv[16];
+  /* clang-format off */
     sprintf(hsv, "h:%3ds:%3dv:%3d",
             rgb_matrix_get_hue(),
             rgb_matrix_get_speed(),
             rgb_matrix_get_val());
-    /* clang-format on */
-    oled_write(hsv, false);
+  /* clang-format on */
+  oled_write(hsv, false);
 }
 
 uint8_t mod_state;
 
 void oled_render_mods(void) {
-    mod_state = get_mods();
-    if (mod_state & MOD_MASK_GUI)
-        oled_write_P(PSTR("super"), super_locked);
-    else
-        oled_write_P(PSTR("     "), false);
-    if (mod_state & MOD_MASK_CTRL)
-        oled_write_P(PSTR(" ctrl"), ctrl_locked);
-    else
-        oled_write_P(PSTR("     "), false);
-    if (mod_state & MOD_MASK_ALT)
-        oled_write_P(PSTR("  alt"), alt_locked);
-    else
-        oled_write_P(PSTR("     "), false);
-    if (mod_state & MOD_MASK_SHIFT)
-        oled_write_P(PSTR("shift"), shift_locked);
-    else
-        oled_write_P(PSTR("     "), false);
+  mod_state = get_mods();
+  if (mod_state & MOD_MASK_GUI)
+    oled_write_P(PSTR("super"), super_locked);
+  else
+    oled_write_P(PSTR("     "), false);
+  if (mod_state & MOD_MASK_CTRL)
+    oled_write_P(PSTR(" ctrl"), ctrl_locked);
+  else
+    oled_write_P(PSTR("     "), false);
+  if (mod_state & MOD_MASK_ALT)
+    oled_write_P(PSTR("  alt"), alt_locked);
+  else
+    oled_write_P(PSTR("     "), false);
+  if (mod_state & MOD_MASK_SHIFT)
+    oled_write_P(PSTR("shift"), shift_locked);
+  else
+    oled_write_P(PSTR("     "), false);
+  if (IS_HOST_LED_ON(USB_LED_CAPS_LOCK))
+    oled_write_P(PSTR(" caps"), true);
+  else
+    oled_write_P(PSTR("     "), false);
 }
 
 void oled_render_logo(void) {
-    /* clang-format off */
+  /* clang-format off */
     static const char PROGMEM logo[] = {
         0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
         0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
         0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
     0 };
-    /* clang-format on */
-    oled_write_P(logo, false);
+  /* clang-format on */
+  oled_write_P(logo, false);
 }
 
 bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        oled_write_P(PSTR("-----"), false);
-        oled_render_layer_state();
-        oled_write_P(PSTR("-----"), false);
-        oled_render_layout();
-        oled_write_P(PSTR("-----"), false);
-        oled_render_rgbinfo();
-        oled_write_P(PSTR("-----"), false);
-        oled_render_mods();
-    } else {
-        oled_render_logo();
-    }
+  if (is_keyboard_master()) {
+    oled_write_P(PSTR("-----"), false);
+    oled_render_layer_state();
+    oled_write_P(PSTR("-----"), false);
+    oled_render_layout();
+    oled_write_P(PSTR("-----"), false);
+    oled_render_rgbinfo();
+    oled_write_P(PSTR("-----"), false);
+    oled_render_mods();
+  } else {
+    oled_render_logo();
+  }
 
   return false;
 }
@@ -396,28 +399,28 @@ HSV blue2  = {180, 255, 127};
 HSV purple = {201, 255, 127};
 HSV pink   = {222, 255, 127};
 
-#    define DEF_S_COEF 1
-#    define DEF_V_COEF .5
+#define DEF_S_COEF 1
+#define DEF_V_COEF .5
 
-#    define C_BASE black
+#define C_BASE black
 
-#    define C_PRIM cyan
-#    define C_PRIM2 purple
-#    define C_SEC purple
-#    define C_SEC2 azure
+#define C_PRIM  cyan
+#define C_PRIM2 purple
+#define C_SEC   purple
+#define C_SEC2  azure
 
-#    define C_MOD yellow
-#    define C_MOD_T orange
+#define C_MOD   yellow
+#define C_MOD_T orange
 
-#    define C_STICKY green
-#    define C_STICKY_T orange
+#define C_STICKY   green
+#define C_STICKY_T orange
 
-#    define C_ESC lgreen
-#    define C_ESC_T orange
+#define C_ESC   lgreen
+#define C_ESC_T orange
 
-#    define C_THUMB white
+#define C_THUMB white
 
-#    define C_KILL red
+#define C_KILL red
 
 /* #    define USE_BL */
 
@@ -430,10 +433,10 @@ void rgb_matrix_indicators_user(void) {
     loop_clrset(R_LET, C_SEC);
     loop_clrset(R_HOME, C_PRIM);
 
-#    ifdef USE_BL
+#ifdef USE_BL
     loop_clrset(L_BL, C_PRIM2);
     loop_clrset(R_BL, C_SEC2);
-#    endif
+#endif
 
     set_clr(L_LF, C_THUMB);
     set_clr(L_RF, C_THUMB);
